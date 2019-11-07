0a680913bcfc3c43976b4289461068679106a0d3
Akhil Khandelwal <akhilkh1989@gmail.com>
2019-11-07 11:27:50 +0530

    skeleton of the project added

---
+++ devops_tut/src/main/java/com/tuts/devops_tut/DevopsTutApplication.java
@@ -0,0 +1,13 @@
+package com.tuts.devops_tut;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTutApplication.class, args);
+	}
+
+}
---
+++ devops_tut/src/test/java/com/tuts/devops_tut/DevopsTutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tuts.devops_tut;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

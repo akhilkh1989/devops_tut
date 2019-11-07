bd69005d366869829859ec608d6e25963453ee1e
Akhil Khandelwal <akhilkh1989@gmail.com>
2019-11-07 11:44:36 +0530

    Hello world scenario committed

---
+++ devops_tut/src/main/java/com/tuts/devops_tut/controller/HelloWorldController.java
@@ -0,0 +1,12 @@
+package com.tuts.devops_tut.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HelloWorldController {
+    @RequestMapping("/")
+    public String helloWorld(){
+        return "index";
+    }
+}
